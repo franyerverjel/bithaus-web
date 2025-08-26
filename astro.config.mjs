92484e531cf7a3910bb6de113fbba10acdd2c3df
Franyer Verjel <franyer.verjel@gmail.com>
2025-08-26 18:13:20 -0500

    Enhance Astro configuration and add new components. Updated dependencies, integrated Tailwind CSS, and added Google Fonts support. Introduced new UI components (Button, Badge, Card) and improved layout with responsive design. Updated site title and description for Bithaus S.A.S. Added new SVG assets and placeholder images. Refactored pages for better content structure and styling.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  experimental: {
+    fonts: [{
+      provider: fontProviders.google(),
+      name: "Inter",
+      cssVariable: "--font-inter"
+    }]
+  }
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Bithaus S.A.S';
+export const SITE_DESCRIPTION = 'Simplificamos lo complejo. Creamos la tecnología que hace tu vida y la de tus clientes más fácil.';
